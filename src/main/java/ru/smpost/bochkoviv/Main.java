9f9438a4fdc2f8b7b685042e69a461cfcc565a8d
Ivan Bochkov <159063277+vanyabooo@users.noreply.github.com>
2024-06-26 15:49:28 +0300

    Update Main.java

--- src/main/java/ru/smpost/bochkoviv/Main.java
+++ src/main/java/ru/smpost/bochkoviv/Main.java
@@ -2,7 +2,7 @@ package ru.smpost.bochkoviv;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa guru!---");
+        System.out.println("Hellooooo qa guru!---");
 
     }
 }
